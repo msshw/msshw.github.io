6ea7c07f9dc3ad3bdea7e9a00477651603d685e6
Maksim Sherstobitow <Maksim.Sherstobitow@trivago.com>
2018-06-17 18:06:02 +0200

    Add event to remove inner menu

--- src/components/page-home.vue
+++ src/components/page-home.vue
@@ -3,3 +3,17 @@
     <router-link :to="'/vue-proper-image/'">Vue Proper Image</router-link>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'PageHome',
+  mounted() {
+    this.updateInnerMenu()
+  },
+  methods: {
+    updateInnerMenu: function() {
+      this.$emit('inner-menu-update', [])
+    }
+  }
+}
+</script>
